5a19245e6e34bdd82f84807854d054decd122086
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-28 15:08:32 -0300

    rota para cadastrar novo usuário

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Post()
+    saveUser(@Body() userDto: CreateUserDto) {
+        return this.userService.saveUser(userDto);
+    }
 
     @UseGuards(JwtAuthGuard)
     @Get('profile')
