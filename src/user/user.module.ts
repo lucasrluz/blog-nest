b8fea07b68659df181adbfd9da756200339d05f4
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-28 10:56:59 -0300

    módulo do usuário

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { userProviders } from './user.providers';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  providers: [UserService],
-  exports: [UserService],
-  controllers: [UserController],
+    imports: [DatabaseModule],
+    providers: [UserService, ...userProviders],
+    exports: [UserService],
+    controllers: [UserController],
 })
 export class UserModule {}
