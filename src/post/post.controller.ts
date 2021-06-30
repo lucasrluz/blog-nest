5e6958af649a65f0a4dc87bfa9fb916b393a3e97
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-30 19:35:32 -0300

    métodos para criar novo post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreatePostDto } from './dto/create-post.dto';
+import { PostService } from './post.service';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+    constructor(private postService: PostService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    savePost(@Request() req, @Body() post: CreatePostDto) {
+        return this.postService.savePost(req.user.id_user, post);
+    }
+}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { User } from 'src/user/entity/user.entity';
+import { Repository } from 'typeorm';
+import { Post } from './entity/post.entity';
+import { IPost } from './interface/post.interface';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(
+        @Inject('POST_REPOSITORY') private postRepository: Repository<Post>,
+        @Inject('USER_REPOSITORY') private userRepository: Repository<User>
+    ) {}
+
+    async savePost(id_user: number, post: IPost) {
+        const postValidate = await this.postRepository.findOne({ title: post.title });
+        
+        if (postValidate !== undefined) {
+            throw new InternalServerErrorException('Este título já está em uso');
+        }
+
+        const user = await this.userRepository.findOne(id_user);
+
+        post.user = user;
+        
+        return this.postRepository.insert(post);
+    }
+}
