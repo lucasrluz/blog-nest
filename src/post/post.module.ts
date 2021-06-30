01478fa6d370e1fae0db78843cbeaedc3582c511
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-30 18:47:50 -0300

    módulo do post

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { userProviders } from 'src/user/user.providers';
 
 @Module({
-  providers: [PostService],
+  providers: [PostService, ...userProviders],
   controllers: [PostController]
 })
 export class PostModule {}
