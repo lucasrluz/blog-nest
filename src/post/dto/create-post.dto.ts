21ceed90e5de428fbe4397a78dcbd21cda3a6fe1
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-30 19:34:37 -0300

    adição do usuário correspondente ao post

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,4 +1,7 @@
+import { User } from "src/user/entity/user.entity";
+
 export class CreatePostDto {
     title: string;
     content: string;
+    user: User;
 }
--- src/post/interface/post.interface.ts
+++ src/post/interface/post.interface.ts
@@ -1,4 +1,7 @@
+import { User } from "src/user/entity/user.entity";
+
 export interface IPost {
     title: string;
     content: string;
+    user: User;
 }
