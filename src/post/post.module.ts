01fae6014da230577d5b5e4e089e7254b6c5488c
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-30 19:36:12 -0300

    adição do provedor do usuário

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { postProviders } from './post.providers';
+import { DatabaseModule } from 'src/database/database.module';
 import { userProviders } from 'src/user/user.providers';
 
 @Module({
-  providers: [PostService, ...userProviders],
-  controllers: [PostController]
+    imports: [DatabaseModule],
+    providers: [PostService, ...postProviders, ...userProviders],
+    controllers: [PostController]
 })
 export class PostModule {}
