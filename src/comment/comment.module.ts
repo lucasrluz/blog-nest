e2a8b0b0ec44d1473d977462ac25beedf43bb1eb
lucasrluz <lucasr.luzbr@gmail.com>
2021-07-03 20:29:41 -0300

    controller para comentário

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CommentService } from './comment.service';
+import { CommentController } from './comment.controller';
 
 @Module({
-  providers: [CommentService]
+  providers: [CommentService],
+  controllers: [CommentController]
 })
 export class CommentModule {}
