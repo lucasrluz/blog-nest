4d1741f7d923cc9e73dd589b5d8cae2223883399
lucasrluz <lucasr.luzbr@gmail.com>
2021-07-03 21:18:19 -0300

    update no módulo do comentário

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { commentProviders } from './comment.providers';
 
 @Module({
-  providers: [CommentService],
-  controllers: [CommentController]
+  imports: [DatabaseModule],
+  providers: [CommentService, ...commentProviders],
+  controllers: [CommentController],
 })
 export class CommentModule {}
