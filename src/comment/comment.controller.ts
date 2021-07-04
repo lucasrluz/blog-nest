213f4266a27aef0950e200ef59d1f3c814472167
lucasrluz <lucasr.luzbr@gmail.com>
2021-07-04 11:36:46 -0300

    método para buscar comentários pelo usuário

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CommentService } from './comment.service';
 
 @Controller('comment')
@@ -8,5 +9,11 @@ export class CommentController {
   @Get('post/:id')
   findByPost(@Param('id') id: number) {
     return this.commentService.findByPost(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('user/:id')
+  findByUser(@Param('id') id: number, @Request() req) {
+    return this.commentService.findByUser(id, req.user.id_user);
   }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Comment } from './entity/comment.entity';
 
@@ -13,5 +13,23 @@ export class CommentService {
     return this.commentRepository.find({
       where: { post: { id_post: id_post } },
     });
+  }
+
+  async findByUser(id_param: number, id_user: number) {
+    if (id_param != id_user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    const comments = await this.commentRepository.find({
+      where: { user: { id_user: id_user } },
+    });
+
+    if (comments === undefined) {
+      throw new NotFoundException(
+        'Nenhum comentário encontrado para ente usuário.',
+      );
+    }
+
+    return comments;
   }
 }
