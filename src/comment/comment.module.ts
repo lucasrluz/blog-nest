fa13b1304be72f96b88798046b4e87a20689f4ca
lucasrluz <lucasr.luzbr@gmail.com>
2021-07-05 10:32:51 -0300

    adição dos provedores de repositórios para usuário e post

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -3,10 +3,17 @@ import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { commentProviders } from './comment.providers';
+import { userProviders } from 'src/user/user.providers';
+import { postProviders } from 'src/post/post.providers';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [CommentService, ...commentProviders],
+  providers: [
+    CommentService,
+    ...commentProviders,
+    ...userProviders,
+    ...postProviders,
+  ],
   controllers: [CommentController],
 })
 export class CommentModule {}
