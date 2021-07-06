eaab80f22d201f81131a4b28165d182f461e23af
lucasrluz <lucasr.luzbr@gmail.com>
2021-07-06 10:18:12 -0300

    novos provedores de repositório

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,17 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { userProviders } from './user.providers';
 import { DatabaseModule } from 'src/database/database.module';
+import { commentProviders } from 'src/comment/comment.providers';
+import { postProviders } from 'src/post/post.providers';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [UserService, ...userProviders],
+  providers: [
+    UserService,
+    ...userProviders,
+    ...commentProviders,
+    ...postProviders,
+  ],
   exports: [UserService],
   controllers: [UserController],
 })
